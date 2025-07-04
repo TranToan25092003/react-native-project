7f511b61d4b5281587b767d6276f9f9b325a5add
unknown <vipboyxu2k3@gmail.com>
2025-07-04 10:20:57 +0700

    setup backend

---
+++ datvexe-backend/functions/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const logRequest = require('../src/middleware/LogRequestMiddleware');
+const passport = require('passport');
+const app = express();
+const server = require('http').createServer(app);
+
+// Middleware xử lý dữ liệu request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logRequest);
+
+// Cấu hình bảo mật, CORS
+app.use(cors({ origin: "*" }));
+require('../src/configs/Auth');
+
+// Kết nối Database
+const { connect } = require('../src/database/db');
+connect();
+
+// Cấu hình Template Engine
+const configViewEngine = require('../src/configs/ViewEngine');
+configViewEngine(app);
+
+// Cấu hình bảo mật
+const security = require('../src/configs/Security');
+security(app);
+
+const initSocket = require("../src/configs/socket");
+initSocket(server)
+
+const setupSwagger = require('../src/swagger/swagger');
+setupSwagger(app);
+
+// Định tuyến chính
+const router = require('../src/routes/MainRouter');
+app.use(router);
+
+const { startServer } = require('../src/configs/PortCustom');
+startServer(server);
