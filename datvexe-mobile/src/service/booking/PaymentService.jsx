19e6c4cc949b564ec8da51de4c3677082055125c
namkzpl <namkz107@gmail.com>
2025-07-13 02:08:51 +0700

    Update PaymentService.jsx

--- datvexe-mobile/src/service/booking/PaymentService.jsx
+++ datvexe-mobile/src/service/booking/PaymentService.jsx
@@ -2,7 +2,10 @@ import fetchWithAuth from "../../utils/fetchWithAuth";
 
 const PaymentService = {
     getUrlVnPayQrCode: (dataReq) => fetchWithAuth(`/payment/create-url-vnpay`, "POST", dataReq),
-    updateStatusPayment: (dataReq) => fetchWithAuth(`/payment/change-status`, "PUT", dataReq),
+    getUrlVnPay: (dataReq) => fetchWithAuth(`/payment/create-url-vnpay`, "POST", dataReq),
+    getUrlVnPayWeb: (dataReq) => fetchWithAuth(`/payment/create-url-vnpay-web`, "POST", dataReq),
+    getUrlVnPayWebQrCode: (dataReq) => fetchWithAuth(`/payment/create-url-vnpay-web-qr-code`, "POST", dataReq),
+    getUrlVnPayWebQrCodeByBookingId: (bookingId) => fetchWithAuth(`/payment/create-url-vnpay-web-qr-code
 }
 
 export default PaymentService;
