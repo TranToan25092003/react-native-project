c5695b77f7db53cf573f0c530ab9b2cbc704615c
HuyenVu <huyenvtthe172681@fpt.edu.vn>
2025-07-13 00:00:14 +0700

    Add StyleSheet for Banner

--- datvexe-mobile/src/components/specific/home/Banner.jsx
+++ datvexe-mobile/src/components/specific/home/Banner.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Image, StyleSheet} from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 const bannerData = [];
 
 const Banner = () => {
     return (
         <Swiper>
-            <Image source={require('../../assets/imageBanner/1.jpg')} style={styles.image} />
-            <Image source={require('../../assets/imageBanner/2.jpg')} style={styles.image} />
-            <Image source={require('../../assets/imageBanner/3.jpg')} style={styles.image} />
-            <Image source={require('../../assets/imageBanner/4.jpg')} style={styles.image} />
-            <Image source={require('../../assets/imageBanner/5.jpg')} style={styles.image} />
-            <Image source={require('../../assets/imageBanner/6.jpg')} style={styles.image} />
+            <Image source={require('../../assets/imageBanner/1.jpg')} style={styles.bannerImage} />
+            <Image source={require('../../assets/imageBanner/2.jpg')} style={styles.bannerImage} />
+            <Image source={require('../../assets/imageBanner/3.jpg')} style={styles.bannerImage} />
+            <Image source={require('../../assets/imageBanner/4.jpg')} style={styles.bannerImage} />
+            <Image source={require('../../assets/imageBanner/5.jpg')} style={styles.bannerImage} />
+            <Image source={require('../../assets/imageBanner/6.jpg')} style={styles.bannerImage} />
         </Swiper>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    bannerImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        borderRadius: 15,
+        opacity: 0.9,
+    }
+});
 
 export default Banner;
