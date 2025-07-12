aaa778518c9ec94acbe08d2b9943a5062f3f7a28
namkzpl <namkz107@gmail.com>
2025-07-13 02:03:20 +0700

    Delete PaymentService.jsx

--- datvexe-mobile/src/service/booking/PaymentService.jsx
+++
@@ -1,8 +0,0 @@
-import fetchWithAuth from "../../utils/fetchWithAuth";
-
-const PaymentService = {
-    getUrlVnPayQrCode: (dataReq) => fetchWithAuth(`/payment/create-url-vnpay`, "POST", dataReq),
-    updateStatusPayment: (dataReq) => fetchWithAuth(`/payment/change-status`, "PUT", dataReq),
-}
-
-export default PaymentService;
