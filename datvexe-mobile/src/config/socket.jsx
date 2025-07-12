14e38cdb998c93ccb62c2b717df5ee600b5380dc
Nguyễn Tiến Đạt <datlacve2@gmail.com>
2025-07-12 17:09:30 +0700

    add file

--- datvexe-mobile/src/config/socket.jsx
+++ datvexe-mobile/src/config/socket.jsx
@@ -1,5 +1,14 @@
 import io from "socket.io-client";
 
-const socket = io("https://api.datvexe-manage.id.vn");
+const socket = io("http://10.0.2.2:9999", {
+  transports: ["websocket"],
+  timeout: 5000, // set timeout ngắn để bắt lỗi nhanh
+});
 
-export default socket;
+socket.on("connect", () => {
+  console.log("✅ Socket connected");
+});
+
+socket.on("connect_error", (err) => {
+  console.error("❌ Socket connect error:", err.message);
+});
